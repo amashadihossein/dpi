4f7daba0e1a616f2f149eb3e1416b164b6258cd1
Clara Amorosi <clara.amorosi@bms.com>
2023-03-15 15:13:28 -0700

    Add local connect test and update man docs

--- tests/testthat/test-dpconnect_check.R
+++ tests/testthat/test-dpconnect_check.R
@@ -2,3 +2,19 @@ test_that("dpconnect_check when not connected", {
   expect_snapshot(dpconnect_check(list(board_alias = "test_board")),
                   error = T)
 })
+
+test_that("dpconnect_check with local board", {
+  # platform agnostic tempdir used for testing
+  board_params <- board_params_set_local(
+    board_alias = "local_test_board",
+    folder = tempdir()
+  )
+  # make sure we can connect first
+  expect_true(dp_connect(board_params = board_params))
+
+  # result from dpconnect_check is a list of class local
+  connect_result <- dpconnect_check(board_params)
+  expect_s3_class(connect_result, "local")
+  expect_named(connect_result, c("board", "name", "cache", "versions"))
+  expect_equal(connect_result$name, "local_test_board")
+})
