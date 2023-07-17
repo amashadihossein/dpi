5ebf368b531c5f897d6c056141d848e5a39abd98
geauxdojang <>
2023-07-17 14:31:59 -0700

    remove dpconnect_check

--- R/util.R
+++ R/util.R
@@ -1,16 +1,1 @@
-#' @title  Check dpconnect executed
-#' @description This checks state whether dpconnect is already executed
-#' @param board_params board_params (only the alias needed)
-#' @keywords internal
-dpconnect_check <- function(board_params) {
-  # board_info <- try(pins::board_get(name = board_params$board_alias), silent = T)
-  board_info <- F
-  if ((board_info)) {
-    stop(cli::format_error(glue::glue(
-      "You are not currently connected to ",
-      "{board_params$board_alias}. Use ",
-      "dp_connect to connect first!"
-    )))
-  }
-  invisible(board_info)
-}
+
