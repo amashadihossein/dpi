a9130582f436da3f7208671ff15c9b99aca30db9
camorosi <50029049+camorosi@users.noreply.github.com>
2023-07-18 15:48:01 -0700

    Add first attempt at dp_connect test for s3

---
+++ tests/testthat/test-dp_connect.R
@@ -0,0 +1,34 @@
+test_that("dp_connect with s3 board", {
+  board_params <- board_params_set_s3(
+    bucket_name = "daapr-test",
+    region = "us-east-1"
+  )
+  creds <- creds_set_aws(
+    key = Sys.getenv("AWS_KEY"),
+    secret = Sys.getenv("AWS_SECRET")
+  )
+
+  # check board object is structured as expected
+  board_obj <- dp_connect(board_params = board_params, creds = creds)
+  expect_s3_class(board_obj, "pins_board_s3")
+  expect_named(board_obj, c('board', 'api', 'cache', 'versioned', 'name',
+                                 'bucket', 'prefix', 'svc'))
+  expect_equal(board_obj$bucket, "daapr-test")
+  expect_equal(board_obj$prefix, "daap/")
+})
+
+# Need to generate snapshot first before adding this test
+# test_that("dp_connect with s3 board with bad creds", {
+#   board_params <- board_params_set_s3(
+#     bucket_name = "daapr-test",
+#     region = "us-east-1"
+#   )
+#   creds <- creds_set_aws(
+#     key = "",
+#     secret = ""
+#   )
+#
+#   # Should get an error message here
+#   expect_snapshot(dp_connect(board_params = board_params, creds = creds),
+#                   error = T)
+# })
